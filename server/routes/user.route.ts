929b24488f5d5d0f560a554afa71f6058dc53eb5
Aniket Panchal <aniketpanchal080@gmail.com>
2024-04-07 11:15:09 +0530

    add accessToken and userInfo function

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import  {activateUser, loginUser, logoutUser, registerUser}  from '../controllers/user.controller'
+import  {activateUser, getUserInfo, loginUser, logoutUser, registerUser, socialAuth, updateAccessToken}  from '../controllers/user.controller'
 import { authorizeRoles, isAuthenticated } from '../middleware/auth'
 
 const userRouter = express.Router()
@@ -8,5 +8,8 @@ userRouter.post('/register',registerUser)
 userRouter.post('/activate-user',activateUser)
 userRouter.post('/login',loginUser)
 userRouter.get('/logout',isAuthenticated,logoutUser)
+userRouter.get('/refresh',updateAccessToken)
+userRouter.get('/me', isAuthenticated,getUserInfo); 
+userRouter.post('/social-auth',socialAuth)
 
 export default userRouter

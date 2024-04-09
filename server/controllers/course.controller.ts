2ff2f2264b2e7329aabc81944844ba2ea398c62b
Aniket Panchal <aniketpanchal080@gmail.com>
2024-04-09 09:09:09 +0530

    add edit_course function

--- server/controllers/course.controller.ts
+++ server/controllers/course.controller.ts
@@ -3,26 +3,62 @@ import { AsyncErrorHandler } from '../middleware/asyncErrorHandler'
 import { NextFunction, Request, Response } from 'express'
 import ErrorHandler from '../config/errorHandler'
 import { createCourse } from '../services/course.service'
+import courseModel from '../models/course.model'
 
 
 //upload course
-export const uploadCourse = AsyncErrorHandler(async(req:Request,res:Response,next:NextFunction)=>{
+export const uploadCourse = AsyncErrorHandler(async (req: Request, res: Response, next: NextFunction) => {
     try {
         const data = req.body;
         const thumbnail = data.thumbnail;
-        
-        if(thumbnail){
-            const myCloud = await cloudinary.v2.uploader.upload(thumbnail,{
-                folder:'lms_courses'
+
+        if (thumbnail) {
+            const myCloud = await cloudinary.v2.uploader.upload(thumbnail, {
+                folder: 'lms_courses'
             })
 
             data.thumbnail = {
-                public_id:  myCloud.public_id,
-                url:myCloud.secure_url
+                public_id: myCloud.public_id,
+                url: myCloud.secure_url
             }
         }
-        createCourse(data,res,next);
-    } catch (error:any) {
-        return next(new ErrorHandler(error.message,400));
+        createCourse(data, res, next);
+    } catch (error: any) {
+        return next(new ErrorHandler(error.message, 400));
     }
+})
+
+
+//edit course
+export const editCourse = AsyncErrorHandler(async (req: Request, res: Response, next: NextFunction) => {
+
+    try {
+
+        const data = req.body;
+
+        // const thumbnail = req.body;
+
+        // if (thumbnail) {
+        //     await cloudinary.v2.uploader.destroy(thumbnail.public_id)
+
+        //     const myCloud = await cloudinary.v2.uploader.upload(thumbnail, {
+        //         folder: "lms_courses"
+        //     })
+
+        //     data.thumbnail = {
+        //         public_id: myCloud.public_id,
+        //         url: myCloud.secure_url
+        //     }
+
+        // }
+
+        const courseId = req.params.id;
+
+        const course = await courseModel.findByIdAndUpdate(courseId, { $set: data }, { new: true })
+
+        res.status(201).json({ success: true, course })
+    } catch (error: any) {
+        return next(new ErrorHandler(error.message, 400))
+    }
+
 })

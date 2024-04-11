9f1112f53d542f23cee02f59d828b90c00434931
Aniket Panchal <aniketpanchal080@gmail.com>
2024-04-11 14:10:41 +0530

    add deleteNotification function

--- server/controllers/notification.controller.ts
+++ server/controllers/notification.controller.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import { AsyncErrorHandler } from "../middleware/asyncErrorHandler";
 import ErrorHandler from "../config/errorHandler";
 import notificationModel from "../models/notification.model";
-
+import cron from 'node-cron'
 
 
 // get all notifications - admin
@@ -42,4 +42,13 @@ export const updateNotification = AsyncErrorHandler(async (req: Request, res: Re
     } catch (error: any) {
         return next(new ErrorHandler(error.message, 400))
     }
+})
+
+
+// delete notifications using cron
+//every mid-night call
+cron.schedule("0 0 0 * * *",async()=>{ 
+    const thirtyDayAge = new Date(Date.now() - 30 * 24* 60 *60 *1000);
+    await notificationModel.deleteMany({status:'read',createdAt: {$lt:thirtyDayAge}})
+    console.log('Delete read notification')
 })
